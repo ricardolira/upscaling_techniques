02502c533a0c0d841183ac35556697c713c9ebb6
Ricardo Lira <rlira@localhost.localdomain>
2017-05-25 13:03:15 -0300

    This is a commit

--- SPE10_block_size.py
+++ SPE10_block_size.py
@@ -29,16 +29,16 @@ def get_block_by_ijk(i, j, k, n_i, n_j, n_k):
     return block
 # Parâmetros
 #outfile_h5m = "multiscale.h5m"
-outfile_vtk = "multiscale.vtk"
+outfile_vtk = "multiscale_CR_3_x_11_17.vtk"
 
 #Get multiscale parameters
 coarse_ratio_x = 3
-coarse_ratio_y = 3
-coarse_ratio_z = 3
+coarse_ratio_y = 22
+coarse_ratio_z = 17
 
-mesh_size_x = 9
-mesh_size_y = 9
-mesh_size_z = 9
+mesh_size_x = 60
+mesh_size_y = 220
+mesh_size_z = 85
 
 mesh_size_x_coarse = mesh_size_x/coarse_ratio_x
 mesh_size_y_coarse = mesh_size_y/coarse_ratio_y
@@ -61,7 +61,7 @@ mb = core.Core()
 root_set = mb.get_root_set()
 mesh_topo_util = topo_util.MeshTopoUtil(mb)
 
-# Vertex generator - Fine scale
+
 coords = np.array([
     (i, j, k) for k in (np.arange(mesh_size_z+1, dtype='float64')*block_size_z/max_mesh_size)
     for j in (np.arange(mesh_size_y+1, dtype='float64')*block_size_y/max_mesh_size)
@@ -94,25 +94,24 @@ coarse_perm_tag = mb.tag_get_handle(
 
 abs_perm_x_tag = mb.tag_get_handle(
     "abs_perm_x", 1, types.MB_TYPE_DOUBLE, True)
-
-#dual_corner_tag = mb.tag_get_handle(
-#    "DUAL_CORNER", 1, types.MB_TYPE_HANDLE, True, storage_type=types.MB_TAG_SPARSE)
+abs_coarse_perm_x_tag = mb.tag_get_handle(
+    "abs_coarse_perm_x", 1, types.MB_TYPE_DOUBLE, True)
 
 #create_wells_by_meshset
-#injection_tag = mb.tag_get_handle(
-#    "injection_well", 1, types.MB_TYPE_INTEGER, True, types.MB_TAG_SPARSE)
-#
-#production_tag = mb.tag_get_handle(
-#    "production_well", 1, types.MB_TYPE_INTEGER, True, types.MB_TAG_SPARSE)
-#
-#injection_wells = {}
-#production_wells = {}
-#injection_wells[1] = mb.create_meshset()
-#
-#production_wells[1] = mb.create_meshset()
-#production_wells[2] = mb.create_meshset()
-#production_wells[3] = mb.create_meshset()
-#production_wells[4] = mb.create_meshset()
+injection_tag = mb.tag_get_handle(
+    "injection_well", 1, types.MB_TYPE_INTEGER, True, types.MB_TAG_SPARSE)
+
+production_tag = mb.tag_get_handle(
+    "production_well", 1, types.MB_TYPE_INTEGER, True, types.MB_TAG_SPARSE)
+
+injection_wells = {}
+production_wells = {}
+injection_wells[1] = mb.create_meshset()
+
+production_wells[1] = mb.create_meshset()
+production_wells[2] = mb.create_meshset()
+production_wells[3] = mb.create_meshset()
+production_wells[4] = mb.create_meshset()
 
 phi_values = []
 with open('spe_phi.dat') as phi:
@@ -130,7 +129,7 @@ with open('spe_perm.dat') as perm:
 perm_values = [float(val) for val in perm_values]
 abs_perm_x = perm_values[0:mesh_size_x*mesh_size_y*mesh_size_z]
 
-#Primal grid and Dual grid generation
+#Primal grid generation
 
 cur_id = 0
 elems = []
@@ -154,6 +153,7 @@ for k, idz in zip(xrange(mesh_size_z), coarse_ids_z):
 
             el = mb.create_element(types.MBHEX, hexa)
 
+#Show fine scale properties
             mb.tag_set_data(gid_tag, el, cur_id)
             mb.tag_set_data(phi_tag, el, phi_values[cur_id])
             mb.tag_set_data(perm_tag, el, [perm_values[cur_id], 0, 0,
@@ -163,6 +163,7 @@ for k, idz in zip(xrange(mesh_size_z), coarse_ids_z):
             cur_id += 1
 
             elems.append(el)
+
 # Create primal coarse grid
             try:
                 primal = primals[(idx, idy, idz)]
@@ -172,39 +173,30 @@ for k, idz in zip(xrange(mesh_size_z), coarse_ids_z):
                 primals[(idx, idy, idz)] = primal
                 mb.add_entities(primal, [el])
 
-#create coarse mesh
-
-
-#for k in xrange()
-
-
-#elems = np.array(elems).reshape(-1, 1)
 
 #setting wells
-#for el in elems[get_block_by_ijk(1, 1, 1, mesh_size_x, mesh_size_y, mesh_size_z)]:
-#    #mb.add_entities(production_wells[1], el)
-#    mb.tag_set_data(production_tag, el, 1)
-#
-#for el in elems[get_block_by_ijk(1, mesh_size_y, 1, mesh_size_x, mesh_size_y, mesh_size_z)]:
-#    #mb.add_entities(production_wells[2], el)
-#    mb.tag_set_data(production_tag, el, 2)
-#
-#for el in elems[get_block_by_ijk(mesh_size_x, mesh_size_y, 1, mesh_size_x, mesh_size_y, mesh_size_z)]:
-#    #mb.add_entities(production_wells[3], el)
-#    mb.tag_set_data(production_tag, el, 3)
-#
-#for el in elems[get_block_by_ijk(mesh_size_x, 1, 1, mesh_size_x, mesh_size_y, mesh_size_z)]:
-#    #mb.add_entities(production_wells[4], el)
-#    mb.tag_set_data(production_tag, el, 4)
-#
-## Esse esta errado:
-##Corrigido [24-04]: Funcinando para quaisquer combinações de nó
-#for el in elems[get_block_by_ijk(mesh_size_x//2, mesh_size_y//2, 1, mesh_size_x, mesh_size_y, mesh_size_z)]:
-#    #mb.add_entities(injection_wells[1], el)
-#    mb.tag_set_data(injection_tag, el, 1)
-#Well positioning by ijk location and attribute tag
-#Set id for global scale mesh
+elems = np.array(elems).reshape(-1, 1)
 
+for el in elems[get_block_by_ijk(1, 1, 1, mesh_size_x, mesh_size_y, mesh_size_z)]:
+    mb.add_entities(production_wells[1], el)
+    mb.tag_set_data(production_tag, el, 1)
+
+for el in elems[get_block_by_ijk(1, mesh_size_y, 1, mesh_size_x, mesh_size_y, mesh_size_z)]:
+    mb.add_entities(production_wells[2], el)
+    mb.tag_set_data(production_tag, el, 2)
+
+for el in elems[get_block_by_ijk(mesh_size_x, mesh_size_y, 1, mesh_size_x, mesh_size_y, mesh_size_z)]:
+    mb.add_entities(production_wells[3], el)
+    mb.tag_set_data(production_tag, el, 3)
+
+for el in elems[get_block_by_ijk(mesh_size_x, 1, 1, mesh_size_x, mesh_size_y, mesh_size_z)]:
+    mb.add_entities(production_wells[4], el)
+    mb.tag_set_data(production_tag, el, 4)
+
+# Esse esta errado:
+for el in elems[get_block_by_ijk(mesh_size_x//2, mesh_size_y//2, 1, mesh_size_x, mesh_size_y, mesh_size_z)]:
+    mb.add_entities(injection_wells[1], el)
+    mb.tag_set_data(injection_tag, el, 1)
 
 coarse_perm={}
 i, j, k = ((1, 0, 0), (0, 1, 0), (0, 0, 1))
@@ -214,37 +206,6 @@ perm_zz = []
 
 meshset_id = 0
 restriction_op = np.zeros((n_coarse,n_fine))
-
-def coarse_centroid(setid):
-    coarse_sums = np.array(
-        [[0, 0, 0],
-        [0, 0, 1],
-        [0, 1, 0],
-        [0, 1, 1],
-        [1, 0, 0],
-        [1, 0, 1],
-        [1, 1, 0],
-        [1, 1, 1]]
-    )
-    coarse_centroid = ((np.asarray(setid) + coarse_sums[0])*np.array([
-        coarse_ratio_x, coarse_ratio_y, coarse_ratio_z]) +
-                      (np.asarray(setid) + coarse_sums[1])*np.array([
-                          coarse_ratio_x, coarse_ratio_y, coarse_ratio_z]) +
-                      (np.asarray(setid) + coarse_sums[2])*np.array([
-                          coarse_ratio_x, coarse_ratio_y, coarse_ratio_z]) +
-                      (np.asarray(setid) + coarse_sums[3])*np.array([
-                          coarse_ratio_x, coarse_ratio_y, coarse_ratio_z]) +
-                      (np.asarray(setid) + coarse_sums[4])*np.array([
-                          coarse_ratio_x, coarse_ratio_y, coarse_ratio_z]) +
-                      (np.asarray(setid) + coarse_sums[5])*np.array([
-                          coarse_ratio_x, coarse_ratio_y, coarse_ratio_z]) +
-                      (np.asarray(setid) + coarse_sums[6])*np.array([
-                          coarse_ratio_x, coarse_ratio_y, coarse_ratio_z]) +
-                      (np.asarray(setid) + coarse_sums[7])*np.array([
-                          coarse_ratio_x, coarse_ratio_y, coarse_ratio_z]))
-
-    coarse_centroid = coarse_centroid // 8
-    return coarse_centroid
 
 min_coarse_ids = np.array([0, 0, 0])
 max_coarse_ids = np.array([max(coarse_ids_x), max(coarse_ids_y), max(coarse_ids_z)])
@@ -256,9 +217,11 @@ for primal_id, primal in primals.iteritems():
     elems_ids = mb.tag_get_data(gid_tag, fine_elems_in_coarse)
     fine_phi_values = mb.tag_get_data(phi_tag, fine_elems_in_coarse)
     coarse_mean_phi = fine_phi_values.mean()
+
     mb.tag_set_data(coarse_phi_tag, primal, coarse_mean_phi)
     mb.tag_set_data(coarse_phi_tag, fine_elems_in_coarse,
-    np.repeat(coarse_mean_phi, len(fine_elems_in_coarse)))
+                    np.repeat(coarse_mean_phi, len(fine_elems_in_coarse)))
+
     fine_perm_values = mb.tag_get_data(perm_tag, fine_elems_in_coarse)
     coarse_perm.update({eid: tensor.reshape(3,3) for eid, tensor in
                         zip(fine_elems_in_coarse, fine_perm_values)
@@ -266,76 +229,37 @@ for primal_id, primal in primals.iteritems():
 
     perm_xx = [(np.dot(np.dot(coarse_perm[elem_id],i),i))
             for elem_id in fine_elems_in_coarse]
+    #perm_xy = [(np.dot(np.dot(coarse_perm[elem_id],i),j))
+    #        for elem_id in fine_elems_in_coarse]
+    #perm_xz = [(np.dot(np.dot(coarse_perm[elem_id],i),k))
+    #        for elem_id in fine_elems_in_coarse]
+
+    #perm_yx = perm_xy
     perm_yy = [(np.dot(np.dot(coarse_perm[elem_id],j),j))
             for elem_id in fine_elems_in_coarse]
+    #perm_yz = [(np.dot(np.dot(coarse_perm[elem_id],j),k))
+    #        for elem_id in fine_elems_in_coarse]
+    #perm_zx = perm_xz
+    #perm_zy = perm_yz
     perm_zz = [(np.dot(np.dot(coarse_perm[elem_id],k),k))
             for elem_id in fine_elems_in_coarse]
-
 #Take average value - upscaling with arithmetic mean
     coarse_perm_xx = np.mean(perm_xx)
     coarse_perm_yy = np.mean(perm_yy)
     coarse_perm_zz = np.mean(perm_zz)
 
-
-##Restriction operator
-#    restriction_op[meshset_id][elems_ids] = 1
-#    meshset_id += 1
-#    #print setid, fine_elems_in_coarse
-#
-## Dual coarse grid
-#    if all(np.array(primal_id) != min_coarse_ids) and all(np.array(primal_id) != max_coarse_ids):
-#        dual_centroid = coarse_centroid(primal_id)
-#    else:
-#        dual_centroid = coarse_centroid(primal_id)
-#
-#        for dim in range(0, 3):
-#            if primal_id[dim] in (0, max_coarse_ids[dim]):
-#                try:
-#                    multiplier = primal_id[dim] / primal_id[dim]
-#                except ZeroDivisionError:
-#                    multiplier = 0
-#
-#                dual_centroid[dim] = multiplier * (mesh_size_x-1)
-#
-#    elem = elems[get_block_by_ijk(
-#        dual_centroid[0]+1, dual_centroid[1]+1, dual_centroid[2]+1,
-#        mesh_size_x, mesh_size_y, dual_centroid[2]+1)[0]]
-#
-#    mb.tag_set_data(dual_corner_tag, elem, primal)
-#
-##for coarse_id_x in (min_coarse_ids[0], max_coarse_ids[0]):
-##    for coarse_id_y in (min_coarse_ids[0], max_coarse_ids[0]):
-##        for coarse_id_z in (min_coarse_ids[0], max_coarse_ids[0]):
-##            coarse_corner((coarse_id_x, coarse_id_y, coarse_id_z))
-#
-#    print primal_id, dual_centroid
-#
-#    #print setid, len(fine_elems_in_coarse), elems_ids
-#
-#
-#for primal_id, primal in primals.iteritems():
-#    print mb.tag_get_data(dual_corner_tag, primal)
+    mb.tag_set_data(abs_coarse_perm_x_tag, primal, coarse_perm_xx)
+    mb.tag_set_data(abs_coarse_perm_x_tag, fine_elems_in_coarse,
+                    np.repeat(coarse_perm_xx, len(fine_elems_in_coarse)))
+    #coarse_perm_tensor = [[coarse_perm_xx, coarse_perm_xy, coarse_perm_xz],
+    #                      [coarse_perm_yx, coarse_perm_yy, coarse_perm_yz],
+    #                      [coarse_perm_zx, coarse_perm_zy, coarse_perm_zz]
+    #                      ]
 
 
-#Prolongation operator
 
 
-# Guilherme
-#for setid, meshset in injection_wells.iteritems():
-#    mb.tag_set_data(injection_tag, meshset, [setid])
-#
-#for setid, meshset in production_wells.iteritems():
-#    mb.tag_set_data(production_tag, meshset, [setid])
-#
-# all_verts = mb.get_entities_by_dimension(0, 0)
-#
-# mesh_topo_util.construct_aentities(all_verts)
-#
-# all_edges = mb.get_entities_by_dimension(0, 1)
-# all_faces = mb.get_entities_by_dimension(0, 2)
-# all_vols = mb.get_entities_by_dimension(0, 3)
-# print len(all_verts), len(all_edges), len(all_faces), len(all_vols)
-#
+
 # Escreve o arquivo
 #mb.write_file(outfile_h5m)
 mb.write_file(outfile_vtk)
